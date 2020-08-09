a8053fc0d8d403cec26575f30ae0ba82c37ba861
Nicolai Negru <negru.n@gmail.com>
2020-08-09 12:44:36 +0100

    Add index.html tests

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+class PagesTests(TestCase):
+
+    def setUp(self):
+        self.response = self.client.get('')
+
+
+    def test_implicit_index_response_200(self):
+        self.assertEqual(200, self.response.status_code)
+
+    def test_index_template_used(self):
+        self.assertTemplateUsed(self.response, 'index.html')
+
+    def test_index_page_contains_movie_id(self):
+        self.assertContains(self.response, 'tt0111161')
