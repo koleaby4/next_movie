ddcb2dea3dad25a0496fd59d238f84ca51c3d255
Nicolai Negru <negru.n@gmail.com>
2020-08-15 16:26:15 +0100

    Format pages > tests.py

--- pages/tests.py
+++ pages/tests.py
@@ -1,20 +1,20 @@
 from django.test import TestCase
 from django.urls import reverse
 
-class PagesTests(TestCase):
 
+class PagesTests(TestCase):
     def test_content_of_index_page(self):
-        url = reverse('index')
+        url = reverse("index")
         response = self.client.get(url)
 
         self.assertEqual(200, response.status_code)
-        self.assertTemplateUsed(response, 'index.html')
-        self.assertContains(response, 'tt0111161')
+        self.assertTemplateUsed(response, "index.html")
+        self.assertContains(response, "tt0111161")
 
     def test_content_of_about_page(self):
-        url = reverse('about')
+        url = reverse("about")
         response = self.client.get(url)
 
         self.assertEqual(200, response.status_code)
-        self.assertTemplateUsed(response, 'about.html')
-        self.assertContains(response, 'On about page')
+        self.assertTemplateUsed(response, "about.html")
+        self.assertContains(response, "On about page")
