f76874fc39bbe28399e12392e19409afc7fb9b85
Nicolai Negru <negru.n@gmail.com>
2020-08-23 07:52:18 +0100

    Add movie model

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from movies.models import Movie
 
-# Register your models here.
+admin.site.register(Movie)
--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,18 @@
+import datetime
+
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+def max_value_current_year(value):
+    next_year = datetime.date.today().year + 1
+    return MaxValueValidator(next_year)(value)
+
+
+class Movie(models.Model):
+    imdb_id = models.CharField(max_length=20)
+    title = models.CharField(max_length=200)
+    year = models.PositiveIntegerField(validators=[MinValueValidator(1900), max_value_current_year])
+    plot = models.CharField(max_length=2000)
+    poster_url = models.URLField()
+    imdb_rating = models.DecimalField(max_digits=3, decimal_places=1)
