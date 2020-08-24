0abc6b4a95e70cde0c05b9a0bb92fd595882a979
Nicolai Negru <negru.n@gmail.com>
2020-08-24 16:06:12 +0100

    Add __str__ to Review model

--- movies/models.py
+++ movies/models.py
@@ -31,3 +31,6 @@ class Review(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name="reviews")
     review = models.CharField(max_length=2000)
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.review
