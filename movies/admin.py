2633d2b1d0bd3dd3a8a1a7ff4421e11ca60546fe
Nicolai Negru <negru.n@gmail.com>
2020-08-24 16:30:34 +0100

    Add Review model

--- movies/admin.py
+++ movies/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from movies.models import Movie
+from movies.models import Movie, Review
+
+class ReviewInline(admin.TabularInline):
+    model = Review
 
 class MovieAdmin(admin.ModelAdmin):
     list_display = ("imdb_id", "title", "year", "imdb_rating")
+    inlines = [
+        ReviewInline,
+    ]
 
 admin.site.register(Movie, MovieAdmin)
--- movies/tests.py
+++ movies/tests.py
@@ -1,11 +1,11 @@
 from django.test import TestCase
 from django.urls import reverse
-from movies.models import Movie
+from django.contrib.auth import get_user_model
+from movies.models import Movie, Review
 import json
 
 
 class PagesTests(TestCase):
-
     def test_content_of_index_page(self):
         url = reverse("movie_list")
         response = self.client.get(url)
@@ -15,17 +15,16 @@ class PagesTests(TestCase):
 
 
 class ModelsTests(TestCase):
-
     def test_valid_Movie_fields(self):
 
         movie = Movie.objects.create(
-            imdb_id = "test_1",
-            title = "Test Title",
-            year = 1900,
-            plot = "Some description of the TEST plot",
-            poster_url = "http://dummy-url.com",
-            imdb_rating = 8.1,
-            full_json_details = '{"imdb_id" : "test_1"}'
+            imdb_id="test_1",
+            title="Test Title",
+            year=1900,
+            plot="Some description of the TEST plot",
+            poster_url="http://dummy-url.com",
+            imdb_rating=8.1,
+            full_json_details='{"imdb_id" : "test_1"}',
         )
 
         self.assertEqual("test_1", movie.imdb_id)
@@ -34,3 +33,26 @@ class ModelsTests(TestCase):
         self.assertEqual("Some description of the TEST plot", movie.plot)
         self.assertEqual("http://dummy-url.com", movie.poster_url)
         self.assertEqual('{"imdb_id" : "test_1"}', movie.full_json_details)
+
+
+class ReviewTests(TestCase):
+    def test_review_creation(self):
+        movie = Movie.objects.create(
+            imdb_id="test_1",
+            title="Test Title",
+            year=1900,
+            plot="Some description of the TEST plot",
+            poster_url="http://dummy-url.com",
+            imdb_rating=8.1,
+            full_json_details='{"imdb_id" : "test_1"}',
+        )
+
+        author = get_user_model().objects.create(
+            username="test_user",
+            email="test_user@email.com",
+            password="Str0ngP@ssw0rd!"
+        )
+
+        review = Review.objects.create(movie=movie, review="This is a test review!", author=author)
+
+        self.assertEqual("This is a test review!", review.review)
