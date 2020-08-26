3a6bf0f0d36fccbd76e97e54d16a59b0cc4683d1
Nicolai Negru <negru.n@gmail.com>
2020-08-26 17:42:19 +0100

    Add Stripe payment button

--- memberships/views.py
+++ memberships/views.py
@@ -1,4 +1,4 @@
-# from django.shortcuts import render
+from project.settings import STRIPE_PUBLIC_KEY
 
 # Create your views here.
 
@@ -6,3 +6,8 @@ from django.views.generic.base import TemplateView
 
 class MembershipsPageView(TemplateView):
     template_name = "memberships/purchase.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["stripe_key"] = STRIPE_PUBLIC_KEY
+        return context
