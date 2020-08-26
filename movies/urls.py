69de93a668d2c883884167dd1d0ff79b6be81600
Nicolai Negru <negru.n@gmail.com>
2020-08-26 08:55:00 +0100

    Index redirects to /movies

--- movies/urls.py
+++ movies/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index, about, MovieListView, MovieDetailView
+
+from .views import MovieDetailView, MovieListView, about
 
 urlpatterns = [
-    path('', MovieListView.as_view(), name="movie_list"),
-    path('<slug:pk>', MovieDetailView.as_view(), name="movie_detail"),
-    path('about', about, name="about"),
+    path("", MovieListView.as_view(), name="movie_list"),
+    path("<slug:pk>", MovieDetailView.as_view(), name="movie_detail"),
+    path("about", about, name="about"),
 ]
--- project/urls.py
+++ project/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path, include
 from movies.views import index
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
-    path('movies/', include('movies.urls')),
-    path('', index, name="index"),
+    path("admin/", admin.site.urls),
+    path("accounts/", include("allauth.urls")),
+    path("movies/", include("movies.urls")),
+    path("", lambda r: redirect("movie_list"), name="index"),
 ]
