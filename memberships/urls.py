39dbdaccc18daa5bb0047261877d6a4f89f3a916
Nicolai Negru <negru.n@gmail.com>
2020-08-27 06:26:16 +0100

    Add stripe payment for membership

--- memberships/urls.py
+++ memberships/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import MembershipsPageView
 
+from .views import MembershipsPageView, charge
 
 urlpatterns = [
-    path("", MembershipsPageView.as_view(), name="memberships")
+    path("", MembershipsPageView.as_view(), name="memberships"),
+    path("charge/", charge, name="charge"),
 ]
--- memberships/views.py
+++ memberships/views.py
@@ -1,13 +1,36 @@
-from project.settings import STRIPE_PUBLIC_KEY
+import stripe
+from django.contrib.auth.models import Permission
+from django.shortcuts import render
+from django.views.generic.base import TemplateView
+
+from project.settings import STRIPE_PUBLISHABLE_KEY, STRIPE_SECRET_KEY
 
 # Create your views here.
 
-from django.views.generic.base import TemplateView
+
+stripe.api_key = STRIPE_SECRET_KEY
+
 
 class MembershipsPageView(TemplateView):
     template_name = "memberships/purchase.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["stripe_key"] = STRIPE_PUBLIC_KEY
+        context["stripe_key"] = STRIPE_PUBLISHABLE_KEY
         return context
+
+
+def charge(request):
+    if request.method == "POST":
+        stripe.Charge.create(
+            amount=999,
+            currency="usd",
+            description="Prime Membership",
+            source=request.POST["stripeToken"],
+        )
+
+        paid_for_membership_permission = Permission.objects.get(codename="paid_for_membership")
+        user = request.user
+        user.user_permissions.add(paid_for_membership_permission)
+
+        return render(request, "memberships/charge.html")
