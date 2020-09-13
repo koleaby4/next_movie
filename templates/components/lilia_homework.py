15aecf26984f75ed1dd278d63c6ef05a826ee14a
Nicolai Negru <negru.n@gmail.com>
2020-09-13 12:59:28 +0100

    Remove redundant file

--- templates/components/lilia_homework.py
+++
@@ -1,8 +0,0 @@
-import random
-
-if __name__ == "__main__":
-    for _ in range(100):
-        first = random.choice([10, 100, 1000, 10000, 1000000])
-        second = random.choice([10, 100, 1000])
-        nr = round(random.random() * first, 4)
-        print(f"{nr:,} * {second:,} = ")
