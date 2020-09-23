c2cf7ada75f53401877493696ac8a7f0b0c37e96
Nicolai Negru <negru.n@gmail.com>
2020-09-23 20:44:42 +0100

    Movie movie signals into signals.py

--- movies/models.py
+++ movies/models.py
@@ -4,14 +4,9 @@ import logging
 import re
 
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import Permission
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
-from django.db.models import Q
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 from django.urls import reverse
-from webpush import send_user_notification
 
 from movies_collector.imdb_collector import get_movie_reviews, url_exists, get_movie_details
 from users.models import CustomUser
@@ -95,44 +90,6 @@ class Movie(models.Model):
 
         return movie
 
-
-@receiver(post_save, sender=Movie)
-def notify_users_of_new_movie(sender, instance, created, **kwargs):
-    payload = {
-        "head": "Another good movie",
-        "body": instance.title,
-        "url": reverse("movie_detail", args=[instance.pk])
-    }
-
-    if instance.poster_url:
-        payload["icon"] = instance.poster_url
-
-
-    log.warning("\n\nMovie's post_save triggered 'notify_users_of_new_movie' function")
-
-    imdb_rating = instance.imdb_rating
-
-    good_movie_imdb_threshold = 7
-    if imdb_rating is None or imdb_rating < good_movie_imdb_threshold:
-        log.warning(f"IMDB rating too low ({imdb_rating}). Skipping notifications.")
-        return
-
-    log.warning(f"Movie {instance} has a rating of {imdb_rating}. Preparing to notify subscribers")
-
-    paid_for_membership_permission = Permission.objects.get(codename="paid_for_membership")
-
-    for user in CustomUser.objects.filter(Q(user_permissions=paid_for_membership_permission)):
-        log.warning(f"\n\nSending push notification to prime membership user: {user}")
-        send_user_notification(user=user, payload=payload)
-
-# fetch and save reviews when a new movie is created
-@receiver(post_save, sender=Movie)
-def persist_reviews(sender, instance, created, **kwargs):
-    reviews = get_movie_reviews(instance.imdb_id)
-    log.warning(f"\n\nFetched reviews: {reviews}")
-
-    for review_details in reviews:
-        Review.from_review_details(instance, review_details)
 
 
 class Review(models.Model):
---
+++ movies/signals.py
@@ -0,0 +1,53 @@
+import logging
+
+from django.contrib.auth.models import Permission
+from django.db.models import Q
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.urls import reverse
+from webpush import send_user_notification
+
+from movies.models import Movie, Review
+from movies_collector.imdb_collector import get_movie_reviews
+from users.models import CustomUser
+
+log = logging.getLogger(__name__)
+
+
+# fetch and save reviews when a new movie is created
+@receiver(post_save, sender=Movie)
+def persist_reviews(sender, instance, created, **kwargs):
+    reviews = get_movie_reviews(instance.imdb_id)
+    log.warning(f"\n\nFetched reviews: {reviews}")
+
+    for review_details in reviews:
+        Review.from_review_details(instance, review_details)
+
+
+@receiver(post_save, sender=Movie)
+def notify_users_of_new_movie(sender, instance, created, **kwargs):
+    payload = {
+        "head": "Another good movie",
+        "body": instance.title,
+        "url": reverse("movie_detail", args=[instance.pk]),
+    }
+
+    if instance.poster_url:
+        payload["icon"] = instance.poster_url
+
+    log.warning("\nMovie's post_save triggered 'notify_users_of_new_movie' function")
+
+    imdb_rating = instance.imdb_rating
+
+    good_movie_imdb_threshold = 7
+    if imdb_rating is None or imdb_rating < good_movie_imdb_threshold:
+        log.warning(f"IMDB rating too low ({imdb_rating}). Skipping notifications.")
+        return
+
+    log.warning(f"Movie {instance} has a rating of {imdb_rating}. Preparing to notify subscribers")
+
+    paid_for_membership_permission = Permission.objects.get(codename="paid_for_membership")
+
+    for user in CustomUser.objects.filter(Q(user_permissions=paid_for_membership_permission)):
+        log.warning(f"Sending push notification to prime membership user: {user}\n")
+        send_user_notification(user=user, payload=payload)
