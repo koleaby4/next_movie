9578059471a400f5ba7247c02da3b12c3f3e2734
Nicolai Negru <negru.n@gmail.com>
2020-09-26 08:57:36 +0100

    Display profile on UserAdmin

--- users/admin.py
+++ users/admin.py
@@ -11,7 +11,7 @@ class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ["email", "username"]
+    list_display = ["email", "username", "profile"]
 
 # Register your models here.
 admin.site.register(CustomUser, CustomUserAdmin)
