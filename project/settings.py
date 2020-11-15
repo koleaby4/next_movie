2455850989ba907d77aef94e60d2df97b69e34aa
Nicolai Negru <negru.n@gmail.com>
2020-11-15 12:22:26 +0000

    Disable debug mode in Django settings

--- project/settings.py
+++ project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).parent.parent
 SECRET_KEY = get_secret("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
 ALLOWED_HOSTS = []
 
